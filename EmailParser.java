826cfd2328fdc156e49799408f4765edbfd09d2d
sk <157430036+shreyakk-1@users.noreply.github.com>
2025-10-06 13:52:23 -0700

    Update EmailParser.java

--- EmailParser.java
+++ EmailParser.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public Class EmailParser{
+  public static void main(String[] args) {
+    Scanner ss = new Scanner(system.in) 
+    System.out.println("type in your email!")
+    g = ss.nextLine();
+    a = g.indexOf("@");
+    b = g.substring(0,(int)(g));
+    t = b.indexOf(".");
+    c = b.substring(0,(int)(t));
+    p = b.length();
+    h = b.substring((int)(t),(int)(p));
+  }
+
+}
